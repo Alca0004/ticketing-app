79c90d72323f1eb6fef3b335ec870ab155a65e8b
alca0004 <alca0004@gmail.com>
2023-11-17 10:45:42 -0500

    Style TicketForm

--- app/(components)/TicketForm.jsx
+++ app/(components)/TicketForm.jsx
@@ -50,6 +50,35 @@ const TicketForm = () => {
                     value={formData.description}
                     rows='5'
                 />
+                <label >Category</label>
+                <select name="category" value={formData.category} onChange={handleChange}>
+                    <option value="Hardware Problem">Hardware Problem</option>
+                    <option value="Software Problem">Software Problem</option>
+                    <option value="Project">Project</option>
+                </select>
+
+                <label>Priority</label>
+                <div>
+                    <input type="radio" id="priority-1 " name="priority" onChange={handleChange} value={1} checked={formData.priority == 1} />
+                    <label>1</label>
+                    <input type="radio" id="priority-2 " name="priority" onChange={handleChange} value={2} checked={formData.priority == 2} />
+                    <label>2</label>
+                    <input type="radio" id="priority-3 " name="priority" onChange={handleChange} value={3} checked={formData.priority == 3} />
+                    <label>3</label>
+                    <input type="radio" id="priority-4 " name="priority" onChange={handleChange} value={4} checked={formData.priority == 4} />
+                    <label>4</label>
+                    <input type="radio" id="priority-5 " name="priority" onChange={handleChange} value={5} checked={formData.priority == 5} />
+                    <label>5</label>
+                </div>
+                <label>Progress</label>
+                <input type="range" id='progress' name="progress" value={formData.progress} min="0" max="100" onChange={handleChange} />
+                <label>Status</label>
+                <select name="status" value={formData.status} onChange={handleChange}>
+                    <option value="not started">Not Started</option>
+                    <option value="started">Started</option>
+                    <option value="done">Done</option>
+                </select>
+                <input type="submit" className="btn " value="Create Ticket" />
             </form>
         </div>
     )
