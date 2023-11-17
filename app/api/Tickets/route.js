4ebc9417147a6e7587894d9bd76da181f90d816e
alca0004 <alca0004@gmail.com>
2023-11-17 13:37:36 -0500

    working on trycatch for the GET function

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,4 +1,4 @@
-import Ticket from '../../(models)/Ticket';
+import Ticket from '@/app/(models)/Ticket';
 import { NextResponse } from 'next/server';
 
 export async function POST(req) {
@@ -8,6 +8,14 @@ export async function POST(req) {
     const ticketData = body.formData;
     await Ticket.create(ticketData);
     return NextResponse.json({ message: 'Ticket Created' }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: 'Error', error }, { status: 500 });
+  }
+}
+
+export async function GET() {
+  try {
+    const tickets = await Ticket.find();
   } catch (error) {
     return NextResponse.json({ message: 'Error', error }, { status: 500 });
   }
