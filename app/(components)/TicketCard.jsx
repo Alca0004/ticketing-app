6c8c58c9e1a8329aa64ccad4ce8d9f397bc47540
alca0004 <alca0004@gmail.com>
2023-11-18 10:32:11 -0500

    time stamp is dynamic

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -4,6 +4,22 @@ import ProgressDisplay from "./ProgressDisplay"
 import StatusDisplay from "./StatusDisplay"
 
 const TicketCard = ({ ticket }) => {
+
+    const formatTimeStamp = (timestamp) => {
+        const options = {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: true
+        };
+        const date = new Date(timestamp)
+        const formattedDate = date.toLocaleString('en-US', options);
+
+        return formattedDate
+    }
+
     return (
         <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
             <div className="flex mb-3 ">
@@ -20,7 +36,7 @@ const TicketCard = ({ ticket }) => {
             <div className="flex-grow"> </div>
             <div className="flex mt-2">
                 <div className="flex flex-col ">
-                    <p className="text-xs my-1">{ticket.createdAt} </p>
+                    <p className="text-xs my-1">{formatTimeStamp(ticket.createdAt)} </p>
                     <ProgressDisplay progress={ticket.progress} />
                 </div>
                 <div className="ml-auto flex items-end">
