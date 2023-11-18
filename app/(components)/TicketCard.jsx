908f74f0ea064bdeb5ee6eafaa7310bcaedccc89
alca0004 <alca0004@gmail.com>
2023-11-18 10:48:39 -0500

    DeleteBlock is dynamic

--- app/(components)/TicketCard.jsx
+++ app/(components)/TicketCard.jsx
@@ -25,7 +25,7 @@ const TicketCard = ({ ticket }) => {
             <div className="flex mb-3 ">
                 <PriorityDisplay priority={ticket.priority} />
                 <div className="ml-auto">
-                    <DeleteBlock />
+                    <DeleteBlock id={ticket._id} />
                 </div>
             </div>
             <h4>{ticket.title}</h4>
